c2b89ea5c76c2c9ce27673dddc12f66a32988f26
CuervoOFC <Ismaelsolis151@gmail.com>
2026-07-30 00:31:07 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,55 @@
+/*
+•❅──────✧✦✧──────❅•
+Codigo Creado Por CUERVO-TEAM-SUPREME
+Para Elymas-Bot Este Codigo Es 
+Exclusivo Y Unico Para Este Bot Al 
+Clonar O Copiar Dejar Estos Creditos 
+De Cuervo-Team-Supreme
+━━━━━ ☾☽ ━━━━━
+ʚĭɞ ೃ CODIGO JAVASCRIPT ʚĭɞ ೃ
+ʚĭɞ ೃ codigo :: config.js
+ʚĭɞ ೃ funcion :: configuracion principal del bot
+ʚĭɞ ೃ estado :: completo
+──────✧✦✧──────
+*/
+
+export const config = {
+
+    botName: 'Cuervo',
+    ownerName: 'Damian',
+
+    owners: [
+        '886958381686',
+        '250405971824657'
+    ],
+
+    prefixes: [
+        '.',
+        '#',
+        '!',
+        '/'
+    ],
+
+    sessionName: './sessions/principal',
+
+    pluginsFolder: './plugins',
+
+    databaseFolder: './database',
+
+    printQRInTerminal: true,
+    loggerLevel: 'silent',
+
+    menu: {
+        title: '🌱 CUERVO BOT',
+        description: 'Bot de WhatsApp modular',
+        footer: '© Cuervo Bot'
+    },
+
+    subbots: {
+        enabled: true,
+        folder: './database/subbots',
+        database: './database/subbots.json'
+    }
+}
+
+export default config
