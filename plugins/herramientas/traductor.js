b162af5da2df4a9b18fc802d1828c494931d4ec0
CuervoOFC <Ismaelsolis151@gmail.com>
2026-08-26 13:20:05 -0600

    Update EVO_KEY in traductor.js

--- plugins/herramientas/traductor.js
+++ plugins/herramientas/traductor.js
@@ -16,7 +16,7 @@ import axios from 'axios'
 import config from '../../config.js'
 import { getSubbotConfig } from '../../lib/subbotconfig.js'
 
-const EVO_KEY = 'evogb-WzR3kPpa'
+const EVO_KEY = 'CuervoOFC'
 
 const LANG_CODES = {
     'abjasio': 'ab', 'achenés': 'ace', 'acholí': 'ach', 'afrikaans': 'af', 'albanés': 'sq',
