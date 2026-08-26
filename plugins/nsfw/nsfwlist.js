9355934ed4099ea345bb06e0a431bbd91b0f526c
CuervoOFC <Ismaelsolis151@gmail.com>
2026-08-26 13:21:28 -0600

    Update API key in nsfwlist.js

--- plugins/nsfw/nsfwlist.js
+++ plugins/nsfw/nsfwlist.js
@@ -17,7 +17,7 @@ import { getSubbotConfig } from '../../lib/subbotconfig.js'
 import { getGroup } from '../../lib/database.js'
 import axios from 'axios'
 
-const API_KEY = 'evogb-WzR3kPpa'
+const API_KEY = 'CuervoOFC'
 const API_BASE_URL = 'https://api.evogb.org/nsfw/interaction'
 
 async function resolveParticipant(rawId, altPn, conn) {
