c2344e3b8e57632351d9c1358082dd0416e96ea7
CuervoOFC <Ismaelsolis151@gmail.com>
2026-08-26 13:22:47 -0600

    Update EVO_KEY for Instagram plugin

--- plugins/stalking/instagram.js
+++ plugins/stalking/instagram.js
@@ -16,7 +16,7 @@ import axios from 'axios'
 import config from '../../config.js'
 import { getSubbotConfig } from '../../lib/subbotconfig.js'
 
-const EVO_KEY = 'evogb-WzR3kPpa'
+const EVO_KEY = 'CuervoOFC'
 const DEFAULT_AVATAR = 'https://i.imgur.com/2w3A80k.jpeg'
 
 async function uploadAvatarUrl(url) {
